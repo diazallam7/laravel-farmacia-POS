63ec7cae3b5dc9172f7765280e7d1a718f26d720
diazallam7 <diazallan124@gmail.com>
2024-08-31 14:02:47 -0400

    empeno

---
+++ database/migrations/2024_08_25_090359_add_columns_to_personas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('personas', function (Blueprint $table) {
+            $table->string('numero_celular')->nullable();
+            $table->string('correo')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('personas', function (Blueprint $table) {
+            $table->dropColumn('numero_celular');
+            $table->dropColumn('correo');
+        });
+    }
+};
